e48cfbb70e1c191b0be34232e267d5f0ad9c302d
Yojan Regmi <devyojan@gmail.com>
2020-04-17 21:22:23 +0400

    courseinfo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,31 +5,31 @@ import Content from './Content';
 import Total from './Total';
 
 const App = () => {
-  const course = 'Half Stack application development'
+  
+  const course = {
+    name: 'Half Stack application development',
 
-  const data = [
-    {
-      part1: 'Fundamentals of React',
-      exercises1: 10,
-    },
-
-    {
-      part2: 'Using props to pass data',
-      exercises2: 7
-    },
-
-    {
-      part3: 'State of a component',
-      exercises3: 14
-    }
-
-  ]
+    parts: [
+      {
+        part1: 'Fundamentals of React',
+        exercises1: 10,
+      },
+      {
+        part2: 'Using props to pass data',
+        exercises2: 7
+      },
+      {
+        part3: 'State of a component',
+        exercises3: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content data={data} />
-      <Total data={data} />
+      <Header course={course.name} />
+      <Content data={course.parts} />
+      <Total data={course.parts} />
     </div>
   )
 }
