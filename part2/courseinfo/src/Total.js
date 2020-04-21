366c20d10bf483d79f31a3dd653ad488bef5060c
Yojan Regmi <devyojan@gmail.com>
2020-04-21 21:24:26 +0400

    courseinfo 2.0

---
+++ part2/courseinfo/src/Total.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Total = (props) => {
+  const total = props.data.reduce((a, b) => a + b.exercises, 0);
+
+  return (
+    <p><strong>total of {total} exercises.</strong></p>
+  );
+}
+
+export default Total;
