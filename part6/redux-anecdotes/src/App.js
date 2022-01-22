3f211d9d658e1f9c93499f739f3f1f7eb3c9a18b
Yojan Regmi <devyojan@gmail.com>
2022-01-22 21:00:47 +0400

    getting anecdotes from the server

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Filter from './components/Filter';
 import Notification from './components/Notification';
+import { useDispatch } from 'react-redux';
+import { initAnecdotes } from './reducers/anecdoteReducer';
+import anecDoteService from './services/anecdote';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecDoteService.getAll().then((anecdotes) => {
+      dispatch(initAnecdotes(anecdotes));
+    });
+  }, [dispatch]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
